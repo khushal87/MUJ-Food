50957354fef6753c46dc688a03af1f0669489abf
rshrc <rishieric91@gmail.com>
2018-10-02 18:24:34 +0530

    Made directory user specific

--- Nightcanteen.py
+++ Nightcanteen.py
@@ -1,12 +1,12 @@
-nc=open('/home/prajay/Documents/NightCanteen.txt')
+nc = open('')  # specify directory
 print("Night Canteen List")
 print("1. Cafe Dialog \n2. Kebab Nation \n3. Login \n4. Saras")
-res=input("\nEnter Night Canteen Hotel Name \n")
-res=res.lower()
-name=list(res)
+res = input("\nEnter Night Canteen Hotel Name \n")
+res = res.lower()
+name = list(res)
 for line in nc:
     for letter in line:
-        if letter==name[0]:
+        if letter == name[0]:
             print(line)
         else:
             break
